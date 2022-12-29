8c27e8504b25680fd712f0648cae3a58df345e82
ryzuwei <junwei.tiu@gmail.com>
2022-12-29 23:58:38 +0800

    1

--- ccass/config.py
+++ ccass/config.py
@@ -1,6 +1,6 @@
-customhost = "employee.cvizqxos8p8v.us-east-1.rds.amazonaws.com"
+customhost = "employee.cpyfr7rv8awg.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "tiujunwei-bucket"
 customregion = "us-east-1"
