6872cd00ab664f4bb97e73fe7c99f16a9a5b59aa
ryzuwei <junwei.tiu@gmail.com>
2023-01-01 17:52:27 +0800

    1

--- config.py
+++
@@ -1,6 +0,0 @@
-customhost = "employee.cpyfr7rv8awg.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "tiujunwei-bucket"
-customregion = "us-east-1"
